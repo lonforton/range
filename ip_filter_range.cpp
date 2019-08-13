5ea9d15d56549c04201c99b47b03f72b98aa43fa
eugene <lonforton@gmail.com>
2019-08-13 14:33:23 +0300

    Now using const auto references in lambdas

--- ip_filter_range.cpp
+++ ip_filter_range.cpp
@@ -27,7 +27,7 @@ IpPool filter(const IpPool& ip_pool, Args... args)
 
   IpPool ip_pool_filtered;
 
-  rs::for_each(ip_pool, [&](auto ip_address) {
+  rs::for_each(ip_pool, [&](const auto& ip_address) {
     if (rs::equal(filter_expression.begin(), filter_expression.end(), ip_address.begin()))
     {
       ip_pool_filtered.push_back(ip_address);
@@ -41,7 +41,7 @@ IpPool filter_any(const IpPool& ip_pool, int filter_expression)
 {  
   IpPool ip_pool_filtered;
   for(auto ip_address : ip_pool    
-     | rv::filter([filter_expression](auto ip_addr)   
+     | rv::filter([filter_expression](const auto& ip_addr)   
                   { 
                      return rs::any_of(ip_addr, [filter_expression](int value) { return value == filter_expression; });
                   })      
@@ -55,9 +55,9 @@ IpPool filter_any(const IpPool& ip_pool, int filter_expression)
 
 void output(const IpPool &ip_pool)
 { 
-  rs::for_each(ip_pool, [](auto ip_address){
+  rs::for_each(ip_pool, [](const auto& ip_address){
     bool first = true;
-    rs::for_each(ip_address, [&first](auto ip_address_part)
+    rs::for_each(ip_address, [&first](const auto& ip_address_part)
     {
       std::cout << (!first ? "." : "") << ip_address_part; 
       first = false; 
@@ -80,7 +80,7 @@ IpPool get_input(std::istream &is)
   {
     IpAddressStr ip_address_str = split(split(line, '\t').at(0), '.');
     IpAddress ip_address;
-    rs::for_each(ip_address_str, [&](auto element) { ip_address.push_back(std::stoi(element));  });
+    rs::for_each(ip_address_str, [&](const auto& element) { ip_address.push_back(std::stoi(element));  });
     ip_pool.push_back(ip_address);
   }
 
