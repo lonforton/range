72b211e8c89a293b4a804e981bf87044867041b7
eugene <lonforton@gmail.com>
2019-08-22 17:29:09 +0300

    time output to std::cerr after each block

--- main.cpp
+++ main.cpp
@@ -8,45 +8,39 @@
 
 #include "ip_filter_range.cpp"
 
-int main(int argc, char *argv[])
+int main()
 {
-  int repeat_number = 1;
-  if (argc > 1)
-    repeat_number = boost::lexical_cast<int>(argv[1]);
-
-  uint64_t full_run_time = 0;
+  auto t1 = std::chrono::high_resolution_clock::now();  
   IpPool ip_pool = get_input();
-
-  for (auto i = 0; i < repeat_number; ++i) {
-    auto t1 = std::chrono::high_resolution_clock::now();
+  std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count() << " - input" << std::endl;  t1 = std::chrono::high_resolution_clock::now();   
 
     try
     {      
       ip_pool = std::move(ip_pool) | ra::sort | ra::reverse;
+      std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count() << " - sort" << std::endl; t1 = std::chrono::high_resolution_clock::now(); 
 
       output(ip_pool);
+      std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count() << " - output" << std::endl; t1 = std::chrono::high_resolution_clock::now(); 
 
-      // TODO filter by first byte and output   // ip = filter(1)
+      // filter by first byte and output   // ip = filter(1)
       IpPool ip_filtered = filter(ip_pool, 1);
       output(ip_filtered);
+      std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count() << " - filter_1" << std::endl; t1 = std::chrono::high_resolution_clock::now(); 
 
-      // TODO filter by first and second bytes and output   // ip = filter(46, 70)
+      // filter by first and second bytes and output   // ip = filter(46, 70)
       ip_filtered = filter(ip_pool, 46, 70);
       output(ip_filtered);
+      std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count() << " - filter_2" << std::endl; t1 = std::chrono::high_resolution_clock::now(); 
 
-      // TODO filter by any byte and output     // ip = filter_any(46)
+      // filter by any byte and output     // ip = filter_any(46)
       ip_filtered = filter_any(ip_pool, 46);
       output(ip_filtered);
+      std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count() << " - filter_3" << std::endl; t1 = std::chrono::high_resolution_clock::now(); 
     }
     catch (const std::exception &e)
     {
       std::cerr << e.what() << std::endl;
     }
-
-    full_run_time += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t1).count();
-  }
-
-  std::cout << "Repeated " << repeat_number << " times, average execution time " << full_run_time / repeat_number << std::endl;
 
   return 0;
 }
